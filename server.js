75b7efd2f6095a68cb52548b03a96218fee31830
Anitha1982 <anithar2003@gmail.com>
2025-11-06 07:46:27 +0530

    Updated project files for redeploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT ||4000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://slooze-dhia.onrender.com/', // frontend served by same server
+  origin: 'https://commodities-api.netlify.app/', // frontend served by same server
   credentials: true
 }));
 app.use(bodyParser.json());
